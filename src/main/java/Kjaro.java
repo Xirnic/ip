a40bf4eb5715e99199d6cc5e5e0e711b243f07ae
Xirnic <juinfeng03@gmail.com>
2025-08-25 17:02:26 +0800

    Level-0

---
+++ src/main/java/Kjaro.java
@@ -0,0 +1,15 @@
+public class Kjaro {
+    public static void main(String[] args) {
+        String logo = " _  __ _                 \r\n" + //
+                        "| |/ /(_) __ _ _ __ ___  \r\n" + //
+                        "| ' / | |/ _` | '__/ _ \\ \r\n" + //
+                        "| . \\ | | (_| | | | (_) |\r\n" + //
+                        "|_|\\_\\/ |\\__,_|_|  \\___/ \r\n" + //
+                        "    |__/                 \n";
+        String line = "___________________________\n";
+        System.out.println(line + logo + line + "Hello! I'm Kjaro\n"
+         + "What can I do for you?\n" + line);
+        
+        System.out.println("That's all? Goodbye!");
+    }
+}
