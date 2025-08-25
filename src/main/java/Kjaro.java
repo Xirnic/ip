8b86ab7941ab099945e31f0129ed956b538e7b38
Xirnic <juinfeng03@gmail.com>
2025-08-25 17:28:23 +0800

    Level-1

--- src/main/java/Kjaro.java
+++ src/main/java/Kjaro.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Kjaro {
+    private static String line = "___________________________\n";
+    private static String indent = "    ";
     public static void main(String[] args) {
         String logo = " _  __ _                 \r\n" + //
                         "| |/ /(_) __ _ _ __ ___  \r\n" + //
@@ -6,10 +10,23 @@ public class Kjaro {
                         "| . \\ | | (_| | | | (_) |\r\n" + //
                         "|_|\\_\\/ |\\__,_|_|  \\___/ \r\n" + //
                         "    |__/                 \n";
-        String line = "___________________________\n";
+       
         System.out.println(line + logo + line + "Hello! I'm Kjaro\n"
          + "What can I do for you?\n" + line);
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            String message = reader.nextLine();
+            if (message.equals("bye")) {
+                reader.close();
+                break;
+            }
+            Echo(message);
+        }
         
-        System.out.println("That's all? Goodbye!");
+        System.out.println(indent + line + indent + "That's all? Goodbye!");
+    }
+
+    public static void Echo(String message) {
+        System.out.println(indent + line + indent + message + "\n" + indent + line);
     }
 }
