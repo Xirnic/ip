056946c69304ee6279ba69569a213593095346be
Xirnic <juinfeng03@gmail.com>
2025-08-27 00:03:50 +0800

    Level-3

--- src/main/java/Kjaro.java
+++ src/main/java/Kjaro.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class Kjaro {
-    private static String line = "___________________________\n";
+    private static String line = "________________________________\n";
+    private static String errLine = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
     private static String indent = "    ";
-    private static String[] listOfItems = new String[100];
+    private static Task[] listOfItems = new Task[100];
     private static int itemsSoFar = 0;
+    public static boolean isRunning = true;
+    public static Scanner reader = new Scanner(System.in);
     public static void main(String[] args) {
         String logo = " _  __ _                 \r\n" + //
                         "| |/ /(_) __ _ _ __ ___  \r\n" + //
@@ -15,30 +18,41 @@ public class Kjaro {
        
         System.out.println(line + logo + line + "Hello! I'm Kjaro\n"
          + "What can I do for you?\n" + line);
-        Scanner reader = new Scanner(System.in);
-        while (true) {
-            String message = reader.nextLine();
-            if (message.equals("bye")) {
-                reader.close();
-                break;
-            }
-            if (message.equals("list")) {
-                PrintList();
-                continue;
-            }
-            Echo("added: " + message);
-            AddToList(message);
-        }
         
-        System.out.println(indent + line + indent + "That's all? Goodbye!");
+        while (isRunning) {
+            String message = reader.nextLine();
+            String[] splitMessage = message.split(" ");
+            if (splitMessage.length == 2 && TwoArgCommand(splitMessage)) continue;
+
+            // Managing single word commands / non-commands
+            switch (message) {
+                case ("bye"):
+                    Farewell();
+                    break;
+                case ("list"):
+                    PrintList();
+                    break;
+                default:
+                    PrintMessage("added: " + message);
+                    AddToList(message);
+            }  
+        }
     }
 
-    public static void Echo(String message) {
-        System.out.println(indent + line + indent + message + "\n" + indent + line);
+    public static void PrintMessage(String... messages) {
+        System.out.println(indent + line);
+        for (int i = 0; i < messages.length; i++) {
+            System.out.println(indent + messages[i]);
+        }
+        System.out.println(indent + line);
+    }
+
+    public static void PrintError(String message) {
+        System.err.println(indent + errLine + indent + message + "\n" + indent + errLine);
     }
 
     public static void AddToList(String message) {
-        listOfItems[itemsSoFar] = message;
+        listOfItems[itemsSoFar] = new Task(message);
         itemsSoFar++;
     }
 
@@ -48,5 +62,48 @@ public class Kjaro {
             System.out.println(indent + (i + 1) + ": " + listOfItems[i]);
         }
         System.out.println(indent + line);
+    }
+
+    public static void Farewell() {
+        PrintMessage("That's all? Goodbye!");
+        reader.close();
+        isRunning = false;
+    }
+    // Managing commands containing two arguments.
+    public static boolean TwoArgCommand(String[] splitMessage) {
+        switch (splitMessage[0]) {
+                case ("mark"):
+                    try {
+                        int taskNumber = Integer.valueOf(splitMessage[1]);
+                        if (taskNumber > itemsSoFar || taskNumber <= 0) {
+                            PrintError("You don't have a task " + taskNumber);
+                            return true;
+                        }
+                        listOfItems[taskNumber - 1].MarkAsDone();
+                        String doneMessage = "Alright! I've marked task " + taskNumber + " as complete!" + "\n";
+                        PrintMessage(doneMessage, listOfItems[taskNumber - 1].toString());
+                    }
+                    catch (NumberFormatException e) {
+                        PrintError("Input a number for marking!");
+                    }
+                    return true;
+
+                case ("unmark"):
+                    try {
+                        int taskNumber = Integer.valueOf(splitMessage[1]);
+                        if (taskNumber > itemsSoFar || taskNumber <= 0) {
+                            PrintError("You don't have a task " + taskNumber);
+                            return true;
+                        }
+                        listOfItems[taskNumber - 1].MarkAsUndone();
+                        String undoneMessage = "Task " + taskNumber + " has been marked undone, you'll get it next time." + "\n";
+                        PrintMessage(undoneMessage, listOfItems[taskNumber - 1].toString());
+                    }
+                    catch (NumberFormatException e) {
+                        PrintError("Input a number for marking!");
+                    }
+                    return true;
+        }
+        return false;
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String taskName;
+    protected boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public void MarkAsDone() {
+        this.isDone = true;
+    }
+
+    public void MarkAsUndone() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        if (this.isDone) {
+            return "[X] " + this.taskName;
+        } else {
+            return "[ ] " + this.taskName;
+        }
+    }
+}
