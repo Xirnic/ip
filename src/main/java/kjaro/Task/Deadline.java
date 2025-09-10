5134e5fd8c19af4441ef7d6dce4b522ebf2968b0
Xirnic <juinfeng03@gmail.com>
2025-09-10 14:37:19 +0800

    Reformat into Packages

---
+++ src/main/java/kjaro/Task/Deadline.java
@@ -0,0 +1,26 @@
+package Kjaro.Task;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Deadline extends Task {
+    private LocalDate dueDate;
+
+    public Deadline(String taskName, LocalDate dueDate) {
+        super(taskName);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() 
+                + " (by: " + dueDate.format(DateTimeFormatter.ofPattern("dd MMM uuuu")) + ")";
+    }
+
+    @Override
+    public String toSave() {
+        return "D/" + super.toSave() 
+                + "/" + dueDate;
+    }
+    
+}
---
+++ src/main/java/kjaro/Task/ToDo.java
@@ -0,0 +1,18 @@
+package Kjaro.Task;
+
+public class ToDo extends Task {
+    
+    public ToDo(String taskName) {
+        super(taskName);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+
+    @Override
+    public String toSave() {
+        return "T/" + super.toSave();
+    }
+}
